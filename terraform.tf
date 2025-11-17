d3bd4cea931adb61bee024a62cf93ab4d5fa8a9d
caietbeyond <carl.pietsch@qbeyond.de>
2025-11-17 15:52:04 +0100

    versions

--- terraform.tf
+++ terraform.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.63.0"
+      version = ">= 4.8.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
-      version = "~> 2.39.0"
+      version = ">= 3.0.2"
     }
   }
 }
